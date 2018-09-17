24bfe051f2b6eaef054f29c0c6b2b8c86be6bc85
Timothy Claeys <timothy.claeys@gmail.com>
2018-09-17 15:19:46 +0200

    Use only 3 serial slots

--- openstack/02b-MAChigh/schedule.h
+++ openstack/02b-MAChigh/schedule.h
@@ -26,7 +26,7 @@ The superframe reappears over time and can be arbitrarily long.
 #define SCHEDULE_MINIMAL_6TISCH_DEFAULT_SLOTFRAME_HANDLE          0 //id of slotframe
 #define SCHEDULE_MINIMAL_6TISCH_DEFAULT_SLOTFRAME_NUMBER          1 //1 slotframe by default.
 
-#define NUMSERIALRX          6
+#define NUMSERIALRX          3
 
 /*
   NUMSLOTSOFF is the max number of cells that the mote can add into schedule, 
